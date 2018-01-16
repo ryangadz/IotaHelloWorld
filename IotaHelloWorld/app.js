fbc0667ded6624dd67283079a405a3d6b87250aa
ryangadz <ryangadz@msn.com>
2018-01-15 16:47:04 -0800

    Connected to Iota full node and received node info

--- IotaHelloWorld/app.js
+++ IotaHelloWorld/app.js
@@ -3,3 +3,18 @@
 //console.log('Hello world');
 console.log('i am 133t u r n00b');
 
+const IOTA = require('iota.lib.js')
+
+// for a list of nodes go to https://iotanode.host/ I am using my personal node on AWS in Oregon// 
+const iota = new IOTA({
+	host: 'http://52.42.145.71',
+	port: 14265
+})
+
+iota.api.getNodeInfo((error, nodeInfo) => {
+	if (error) {
+		console.error('getNodeInfo error', error)
+	} else {
+		console.log('getNodeInfo result', nodeInfo)
+	}
+})
