2e9f0183603c679eb1d5c233c34b1546b3c5edc9
ryangadz <ryangadz@msn.com>
2018-01-16 17:28:36 -0800

    connected to test node and send a hello world message

--- IotaHelloWorld/app.js
+++ IotaHelloWorld/app.js
@@ -5,9 +5,10 @@ console.log('i am 133t u r n00b');
 
 const IOTA = require('iota.lib.js')
 
-// for a list of nodes go to https://iotanode.host/ I am using my personal node on AWS in Oregon// 
+// for a list of nodes go to https://iotanode.host/ I am using my personal node on AWS in Oregon
 const iota = new IOTA({
-	host: 'http://52.42.145.71',
+	//host: 'http://52.42.145.71', doesnt currently work with iota.api.sendTransfer
+    host: 'http://node.iotawallet.info',
 	port: 14265
 })
 
@@ -18,3 +19,36 @@ iota.api.getNodeInfo((error, nodeInfo) => {
 		console.log('getNodeInfo result', nodeInfo)
 	}
 })
+
+var seed = 'W99RJ9M9A9IKOHB9BFGDKLP9CORGD9HHKQ99BNEWUVI9QVQOOVH9GQN9DDFHQJMJT9O9BQOWOMY9EJX9Y' // keep it secure!
+var options = {
+    index: 3,
+    checksum: true
+}
+
+
+
+
+iota.api.getNewAddress(seed, options, function (error, address) {
+    // attach the address to the tangle with message
+    var transfer = [{
+        address: address,
+        value: 0,
+        message: iota.utils.toTrytes('Hello World!'),
+        tag: ''
+    }]
+    // depth for the tip selection
+    var depth = 4;
+    // If we're on the mainnet, minWeightMagnitude is 18
+    var minWeightMagnitude = 18;
+    // Call the sendTransfer API wrapper function 
+    // It takes care prepareTransfers, attachToTangle, broadcast and storeTransactions
+    iota.api.sendTransfer(seed, depth, minWeightMagnitude, transfer, function (e, attached) {
+        if (!e) {
+            console.log("successfully attached", attached);
+        }
+        else console.log("failed sad face");
+    })
+});
+
+
